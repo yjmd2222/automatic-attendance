99d8cdb7ee04658f3e40c108d9d0198f3e834452
jinmo <yjmd2222@gmail.com>
2023-06-06 14:50:04 +0900

    Update info-template.py accordingly

--- info-template.py
+++ info-template.py
@@ -1,3 +1,4 @@
 GMAIL_APP_PASSWORD = 'password'
 EMAIL_ADDRESS = 'whatisit@gmail.com'
-CHROME_EXTENSION_LINK = 'chrome-extension://ekoaehpknadfoaolagjfdefeopkhfhln/src/popup/popup.html'
+SCREEN_QR_READER_POPUP_LINK = 'chrome-extension://ekoaehpknadfoaolagjfdefeopkhfhln/src/popup/popup.html'
+SCREEN_QR_READER_SOURCE = './extension_0_1_2_0.crx'
