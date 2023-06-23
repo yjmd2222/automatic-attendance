82d9af3feb6d3f74741911a25bc8f999a9071cf1
yjmd2222 <122770065+yjmd2222@users.noreply.github.com>
2023-06-23 16:46:37 +0900

    Wrap each Selenium activity with try and except and use loops inside methods instead of decorators (#35)
    * try except and repeat selenium processes
    
    * delete commented lines
    
    * Do switch the description back
    
    * Change get_link to check_link as return is boolean
    
    makes more sense i think
    
    * don't know why but didn't recognize qr code
    
    repeat by preserving aspect ratio
    
    * how='submit', not 'iframe'
    
    I specifically wrote that in the description but used the wrong input lol
    
    * Need to carefully read what I wrote
    
    * Deleted decorators and made wraps in methods

--- fake_attendance/settings.py
+++ fake_attendance/settings.py
@@ -33,13 +33,12 @@ START_IMAGE = get_file_path('start_video.png', 'images')
 # 11:00, 13:00, 15:30 project day 1 - ?
 DIFF_MINUTE = 5
 CHECK_IN_TIMES = [get_time_sets(*TIME_SET, DIFF_MINUTE) for TIME_SET in\
-                  [(10,0), (10,30), (11, 0), (11,20), (13,11), (14,30), (15,20), (17,0)]]
+                  [(11,20), (13,11), (14,30), (15,20), (17,0)]]
 CHECK_IN_TIMES = [TIME_SET for TIME_SETS in CHECK_IN_TIMES for TIME_SET in TIME_SETS]
 ZOOM_ON_HOURS = '9,13'
 ZOOM_QUIT_HOURS = '12,18'
 
 # Zoom props
-ZOOM_RESIZE_PARAMETERS_LIST = [i/10 for i in range(3,11)] # Change Zoom window size
 ZOOM_AGREE_RECORDING_POPUP_CLASS = 'ZPRecordingConsentClass' # '이 회의는 호스트 또는 참가자가 기록 중입니다'
 ZOOM_CLASSROOM_CLASS = 'ZPContentViewWndClass' # 'Zoom 회의'
 ZOOM_LAUNCHING_CHROME_TITLE = '회의 시작 - Zoom - Chrome'
@@ -49,6 +48,7 @@ LOGIN_WITH_KAKAO_BUTTON = 'login-form__button-title.css-caslt6'
 ID_INPUT_BOX = 'loginKey--1'
 PASSWORD_INPUT_BOX = 'password--2'
 LOGIN_BUTTON = 'btn_g.highlight.submit'
+IFRAME = 'iframe'
 AGREE = "//*[text()='동의합니다.']"
 CHECK_IN = "//*[text()='출석']"
 SUBMIT = "//*[text()='제출']"
