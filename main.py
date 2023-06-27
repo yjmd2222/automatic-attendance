3f68acb6bf5b9f1a95d739bb6d71d6e0ba00222d
jinmo <yjmd2222@gmail.com>
2023-06-27 21:07:33 +0900

    Clean up stats branch

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
+'''
+MIT License
+
+Copyright (c) 2021 Monirul Shawon
+Copyright (c) 2023 yjmd2222
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Python script to update traffic data on given repository
+Original code by @MShawon
+Modified by @yjmd2222 to support total view counts as well as latest clone and view counts
+'''
 import json
 
 def load_json(stats):
+    'loads newly pulled and saved json'
     with open(f'{stats}.json', 'r') as fh:
         now = json.load(fh)
     with open(f'{stats}_before.json', 'r') as fh:
@@ -8,9 +37,11 @@ def load_json(stats):
     return now, before
 
 def timestamp_list(stats, before):
+    'makes list of timestamps to compare new and saved json data by date'
     return {before[stats][i]['timestamp']: i for i in range(len(before[stats]))}
 
 def append_json(stats, timestamps, before, now):
+    'uses above timestamp list to merge new data into the previous data'
     latest = dict(before)
     for i in range(len(now[stats])):
         timestamp = now[stats][i]['timestamp']
@@ -21,13 +52,18 @@ def append_json(stats, timestamps, before, now):
     return latest
 
 def sum_counts(latest, stats):
+    'sum merged counts'
     latest['count'] = sum(map(lambda x: int(x['count']), latest[stats]))
     latest['uniques'] = sum(map(lambda x: int(x['uniques']), latest[stats]))
     return latest
 
-sort_key = lambda x: x['timestamp']
+sort_key = lambda x: x['timestamp'] # sort by time
 
 def merge_counts(timestamps, stats_var):
+    '''
+    merge past data if data gets too long
+    It currently supports up to 100 months
+    '''
     if len(timestamps) > 100:
         remove_this = []
         stats_var.sort(key=sort_key)
@@ -44,10 +80,12 @@ def merge_counts(timestamps, stats_var):
     return stats_var
 
 def dump_json(stats, latest):
+    'dump processed data back to json'
     with open(f'{stats}.json', 'w', encoding='utf-8') as fh:
         json.dump(latest, fh, ensure_ascii=False, indent=4)
 
 def run(stats):
+    'runs the whole process'
     now, before = load_json(stats)
     timestamps = timestamp_list(stats, before)
     latest = append_json(stats, timestamps, before, now)
