504b7cc63e4a59bc9a89ce6c1cc3fbce989553e6
jinmo <yjmd2222@gmail.com>
2023-06-27 15:29:14 +0900

    main goes in stats, stats.yml goes in main

--- main.py
+++ main.py
@@ -1,61 +1,61 @@
 import json
 
-def load_json(traffic):
-    with open(f'{traffic}.json', 'r') as fh:
+def load_json(stats):
+    with open(f'{stats}.json', 'r') as fh:
         now = json.load(fh)
-    with open(f'{traffic}_before.json', 'r') as fh:
+    with open(f'{stats}_before.json', 'r') as fh:
         before = json.load(fh)
     return now, before
 
-def timestamp_list(traffic, before):
-    return {before[traffic][i]['timestamp']: i for i in range(len(before[traffic]))}
+def timestamp_list(stats, before):
+    return {before[stats][i]['timestamp']: i for i in range(len(before[stats]))}
 
-def append_json(traffic, timestamps, before, now):
+def append_json(stats, timestamps, before, now):
     latest = dict(before)
-    for i in range(len(now[traffic])):
-        timestamp = now[traffic][i]['timestamp']
+    for i in range(len(now[stats])):
+        timestamp = now[stats][i]['timestamp']
         if timestamp in timestamps:
-            latest[traffic][timestamps[timestamp]] = now[traffic][i]
+            latest[stats][timestamps[timestamp]] = now[stats][i]
         else:
-            latest[traffic].append(now[traffic][i])
+            latest[stats].append(now[stats][i])
     return latest
 
-def sum_counts(latest, traffic):
-    latest['count'] = sum(map(lambda x: int(x['count']), latest[traffic]))
-    latest['uniques'] = sum(map(lambda x: int(x['uniques']), latest[traffic]))
+def sum_counts(latest, stats):
+    latest['count'] = sum(map(lambda x: int(x['count']), latest[stats]))
+    latest['uniques'] = sum(map(lambda x: int(x['uniques']), latest[stats]))
     return latest
 
 sort_key = lambda x: x['timestamp']
 
-def merge_counts(timestamps, traffic_var):
+def merge_counts(timestamps, stats_var):
     if len(timestamps) > 100:
         remove_this = []
-        traffic_var.sort(key=sort_key)
+        stats_var.sort(key=sort_key)
         for i in range(len(timestamps) - 35):
-            traffic_var[i]['timestamp'] = traffic_var[i]['timestamp'][:7] 
-            if traffic_var[i]['timestamp'] == traffic_var[i+1]['timestamp'][:7]:
-                traffic_var[i+1]['count'] +=  traffic_var[i]['count']
-                traffic_var[i+1]['uniques'] +=  traffic_var[i]['uniques']
-                remove_this.append(traffic_var[i])
+            stats_var[i]['timestamp'] = stats_var[i]['timestamp'][:7] 
+            if stats_var[i]['timestamp'] == stats_var[i+1]['timestamp'][:7]:
+                stats_var[i+1]['count'] +=  stats_var[i]['count']
+                stats_var[i+1]['uniques'] +=  stats_var[i]['uniques']
+                remove_this.append(stats_var[i])
 
         for item in remove_this:
-            traffic_var.remove(item)
+            stats_var.remove(item)
 
-    return traffic_var
+    return stats_var
 
-def dump_json(traffic, latest):
-    with open(f'{traffic}.json', 'w', encoding='utf-8') as fh:
+def dump_json(stats, latest):
+    with open(f'{stats}.json', 'w', encoding='utf-8') as fh:
         json.dump(latest, fh, ensure_ascii=False, indent=4)
 
-def run(traffic):
-    now, before = load_json(traffic)
-    timestamps = timestamp_list(traffic, before)
-    latest = append_json(traffic, timestamps, before, now)
-    latest = sum_counts(latest, traffic)
-    traffic_var = latest[traffic]
-    traffic_var = merge_counts(timestamps, traffic_var)
-    traffic_var.sort(key=sort_key, reverse=True)
-    dump_json(traffic, latest)
+def run(stats):
+    now, before = load_json(stats)
+    timestamps = timestamp_list(stats, before)
+    latest = append_json(stats, timestamps, before, now)
+    latest = sum_counts(latest, stats)
+    stats_var = latest[stats]
+    stats_var = merge_counts(timestamps, stats_var)
+    stats_var.sort(key=sort_key, reverse=True)
+    dump_json(stats, latest)
 
-for traffic in ['clones', 'views']:
-    run(traffic)
+for stats in ['clones', 'views']:
+    run(stats)
