147611166773d98d230a2ac211117278727a77dd
jinmo <yjmd2222@gmail.com>
2023-06-28 18:43:53 +0900

    codestyle

--- main.py
+++ main.py
@@ -30,10 +30,10 @@ import json
 
 def load_json(stats):
     'loads newly pulled and saved json'
-    with open(f'{stats}.json', 'r') as fh:
-        now = json.load(fh)
-    with open(f'{stats}_before.json', 'r') as fh:
-        before = json.load(fh)
+    with open(f'{stats}.json', 'r', encoding='utf-8') as file:
+        now = json.load(file)
+    with open(f'{stats}_before.json', 'r', encoding='utf-8') as file:
+        before = json.load(file)
     return now, before
 
 def timestamp_list(stats, before):
@@ -57,7 +57,9 @@ def sum_counts(latest, stats):
     latest['uniques'] = sum(map(lambda x: int(x['uniques']), latest[stats]))
     return latest
 
-sort_key = lambda x: x['timestamp'] # sort by time
+def sort_key(item):
+    'sort by time'
+    return item['timestamp']
 
 def merge_counts(timestamps, stats_var):
     '''
@@ -68,7 +70,7 @@ def merge_counts(timestamps, stats_var):
         remove_this = []
         stats_var.sort(key=sort_key)
         for i in range(len(timestamps) - 35):
-            stats_var[i]['timestamp'] = stats_var[i]['timestamp'][:7] 
+            stats_var[i]['timestamp'] = stats_var[i]['timestamp'][:7]
             if stats_var[i]['timestamp'] == stats_var[i+1]['timestamp'][:7]:
                 stats_var[i+1]['count'] +=  stats_var[i]['count']
                 stats_var[i+1]['uniques'] +=  stats_var[i]['uniques']
@@ -81,8 +83,8 @@ def merge_counts(timestamps, stats_var):
 
 def dump_json(stats, latest):
     'dump processed data back to json'
-    with open(f'{stats}.json', 'w', encoding='utf-8') as fh:
-        json.dump(latest, fh, ensure_ascii=False, indent=4)
+    with open(f'{stats}.json', 'w', encoding='utf-8') as file:
+        json.dump(latest, file, ensure_ascii=False, indent=4)
 
 def run(stats):
     'runs the whole process'
@@ -95,5 +97,5 @@ def run(stats):
     stats_var.sort(key=sort_key, reverse=True)
     dump_json(stats, latest)
 
-for stats in ['clones', 'views']:
-    run(stats)
+for statistics in ['clones', 'views']:
+    run(statistics)
