19f2988ec5e8c3b7be74a642ee152d6c8cec2b57
jinmo <yjmd2222@gmail.com>
2023-07-14 09:18:39 +0900

    Added -e --extrapolate argument to apply 5-minute-offsets logic to time inputs

--- fake_attendance/helper.py
+++ fake_attendance/helper.py
@@ -48,6 +48,10 @@ def convert_to_datetime(hour, minute=0, second=0):
     'convert given time to datetime object'
     return datetime.now().replace(hour=hour, minute=minute, second=second)
 
+def convert_to_str(time):
+    'convert given datetime to str'
+    return time.hour, time.minute, time.second
+
 def extrapolate_time_sets(hour, minute, second=0, diff_minute=5):
     'return time sets diff_min and 2*diff_min minutes before and after given time'
     input_time = convert_to_datetime(hour, minute, second)
