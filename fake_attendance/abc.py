yjmd2222/automatic-attendance

commit 007e5a6bf72d47fc37f351350e935e62601ba919

Reduced code for sending email by including the send code in the decorator (#57)
* fix indendation in email body

* Use decorators to send email and reset attributes

* decorator inside class

fake_attendance/abc.py
@@ -13,21 +13,20 @@ import win32con
 import win32gui
 
 # pylint: disable=wrong-import-position
-from fake_attendance.arg_parse import args
-from fake_attendance.helper import decorator_start_end
+from fake_attendance.arg_parse import parsed_args
+from fake_attendance.helper import print_with_time
 from fake_attendance.settings import VERBOSITY__INFO
 # pylint: enable=wrong-import-position
 
-# pylint: disable=too-few-public-methods
 class BaseClass(ABC):
     'base class for abstraction'
 
     # pylint: disable=no-member
     @abstractmethod
     def __init__(self):
-        'BaseClass.__init__(self) method that decorates self.run to print start and end of it'
+        'BaseClass.__init__() that decorates self.run() to print start and end of it'
         # to be used with super().__init__() in subclass
-        self.run = decorator_start_end(self.print_name)(self.run)
+        self.run = self.decorator_start_end(self.print_name)(self.run)
     # pylint: enable=no-member
 
     # pylint: disable=method-hidden
@@ -35,7 +34,19 @@ class BaseClass(ABC):
     def run(self):
         'run everything'
     # pylint: enable=method-hidden
-# pylint: enable=too-few-public-methods
+
+    @staticmethod
+    def decorator_start_end(name):
+        'outer decorator, argument should be name for script'
+        def inner_decorator(func):
+            'inner'
+            def wrapper(*args, **kwargs):
+                'wrapper'
+                print_with_time(f'{name} 스크립트 시작')
+                func(*args, **kwargs)
+                print_with_time(f'{name} 스크립트 종료')
+            return wrapper
+        return inner_decorator
 
 class UseSelenium(BaseClass):
     'base class for subclasses that use Selenium'
@@ -43,13 +54,14 @@ class UseSelenium(BaseClass):
     @abstractmethod
     def __init__(self):
         '''
-        UseSelenium.__init__(self) method that defines an empty Selenium self.driver attribute.\n
-        Also inherits from BaseClass.__init__() method that decorates self.run to print start and\n
+        UseSelenium.__init__() that defines an empty Selenium self.driver\n
+        and self.verbosity attributes.\n
+        Also inherits from BaseClass.__init__() that decorates self.run to print start and\n
         end of it
         '''
         super().__init__()
         self.driver = None
-        self.verbosity = args.verbosity
+        self.verbosity = parsed_args.verbosity
 
     def create_selenium_options(self):
         '''

fake_attendance/arg_parse.py
@@ -18,4 +18,4 @@ parser.add_argument('-e', '--extrapolate', action='store_true',
                     help='입력한 시간 포함 주위 5분, 10분 단위로 출석 체크')
 parser.add_argument('-v', '--verbosity', type=int, default=VERBOSITY__INFO, help='출력메시지 선택. 기본값 3')
 
-args = parser.parse_args()
+parsed_args = parser.parse_args()

fake_attendance/fake_check_in.py
@@ -34,11 +34,11 @@ from fake_attendance.settings import (
     AGREE,
     CHECK_IN,
     SUBMIT)
-from fake_attendance.notify import SendEmail
+from fake_attendance.notify import PrepareSendEmail
 # pylint: enable=wrong-import-position
 
 # pylint: disable=too-many-instance-attributes
-class FakeCheckIn(SendEmail, UseSelenium):
+class FakeCheckIn(PrepareSendEmail, UseSelenium):
     'A class for checking QR image and sending email with link'
 
     def __init__(self, sched_drop_runs_until=None):
@@ -50,7 +50,8 @@ class FakeCheckIn(SendEmail, UseSelenium):
         self.extension_source = SCREEN_QR_READER_SOURCE
         self.is_wait = False
         self.print_name = 'QR 체크인'
-        SendEmail.__init__(self)
+        PrepareSendEmail.define_attributes(self)
+        PrepareSendEmail.decorate_run(self)
         UseSelenium.__init__(self)
 
     def reset_attributes(self):
@@ -59,7 +60,7 @@ class FakeCheckIn(SendEmail, UseSelenium):
         self.rect = self.maximize_window(self.hwnd) if self.is_window else [100,100,100,100]
         self.driver = None
         self.is_wait = False
-        SendEmail.__init__(self)
+        PrepareSendEmail.define_attributes(self)
 
     def check_window(self):
         'check and return window'
@@ -230,10 +231,6 @@ class FakeCheckIn(SendEmail, UseSelenium):
 
         return is_continue
 
-    def print_wont_run_until(self, until):
-        'print that check-in will be ignored until given time'
-        print_with_time(f'기존 출석 확인. {datetime.strftime(until, "%H:%M")}까지 출석 체크 실행 안 함')
-
     def run(self):
         'run whole check-in process'
         # get Zoom window
@@ -263,16 +260,13 @@ class FakeCheckIn(SendEmail, UseSelenium):
         print_with_time('QR 코드 확인. 출석 체크 진행')
         check_in_result = self.check_in()
 
-        # send email
+        # update result
         if check_in_result:
             self.result_dict['result']['content'] = '성공'
             self.is_wait = True
         else:
             self.result_dict['result']['content'] = '실패'
             self.is_wait = False
-        self.notify.record_result(self.result_dict)
-        self.notify.write_body()
-        self.notify.run()
 
         # quit Selenium
         self.driver.quit()
@@ -284,8 +278,7 @@ class FakeCheckIn(SendEmail, UseSelenium):
         if self.is_wait and self.sched_drop_runs_until:
             until = datetime.now() + timedelta(minutes=30)
             self.sched_drop_runs_until(self.print_name, until)
-            self.print_wont_run_until(until)
-        self.reset_attributes()
+            print_with_time(f'출석 확인. {datetime.strftime(until, "%H:%M")}까지 출석 체크 실행 안 함')
         return
 # pylint: enable=too-many-instance-attributes
 

fake_attendance/launch_zoom.py
@@ -21,7 +21,7 @@ from fake_attendance.helper import (
     print_all_windows,
     send_alt_key_and_set_foreground)
 from fake_attendance.quit_zoom import QuitZoom
-from fake_attendance.notify import SendEmail
+from fake_attendance.notify import PrepareSendEmail
 from fake_attendance.settings import (
     ZOOM_AGREE_RECORDING_POPUP_CLASS,
     ZOOM_CLASSROOM_CLASS,
@@ -30,20 +30,21 @@ from fake_attendance.settings import (
     ZOOM_UPDATE_ACTUAL_UPDATE_CLASS)
 # pylint: enable=wrong-import-position
 
-class LaunchZoom(SendEmail, UseSelenium):
+class LaunchZoom(PrepareSendEmail, UseSelenium):
     'A class for launching Zoom'
 
     def __init__(self):
         'initialize'
         self.hwnd_zoom_classroom = 0
         self.print_name = '줌 실행'
-        SendEmail.__init__(self)
+        PrepareSendEmail.define_attributes(self)
+        PrepareSendEmail.decorate_run(self)
         UseSelenium.__init__(self)
 
     def reset_attributes(self):
         'reset attributes for next run'
         self.hwnd_zoom_classroom = 0
-        SendEmail.__init__(self)
+        PrepareSendEmail.define_attributes(self)
 
     def quit_zoom(self):
         'quit hidden Zoom windows if any'
@@ -201,15 +202,9 @@ class LaunchZoom(SendEmail, UseSelenium):
             self.process_popup(ZOOM_AGREE_RECORDING_POPUP_CLASS, reverse=True, send_alt=True)
             print_with_time('동의 재확인')
             self.process_popup(ZOOM_AGREE_RECORDING_POPUP_CLASS, reverse=True, send_alt=True)
-        # send email
-        self.notify.record_result(self.result_dict)
-        self.notify.write_body()
-        self.notify.run()
         # maximize if everything done correctly
         if self.result_dict[ZOOM_AGREE_RECORDING_POPUP_CLASS]['content']:
             self.maximize_window(self.hwnd_zoom_classroom)
-        # reset attributes for next run
-        self.reset_attributes()
 
 if __name__ == '__main__':
     LaunchZoom().run()

fake_attendance/notify.py
@@ -33,34 +33,29 @@ class Notify(BaseClass):
     def __init__(self, job_id='job_id'):
         'initialize'
         self.link = ''
-        self.result = ''
         self.job_id = job_id
-        self.body = ''
         self.print_name = '이메일 발송'
         super().__init__()
 
-    def record_result(self, result):
-        'record result'
-        self.result = result
-
-    def write_body(self):
+    def write_body(self, result):
         'write email body with result'
-        # unfoiling nested dictionary: 'item1: result1\nitem2: result2'
-        total_result = []
-        for _, ovalue in self.result.items():
-            single_result = []
-            for ivalue in ovalue.values():
-                single_result.append(str(ivalue))
-            total_result.append(': '.join(single_result))
-        result = '\n'.join(total_result)
+        if isinstance(result, dict):
+            # unfoiling nested dictionary: 'item1: result1\nitem2: result2'
+            total_result = []
+            for _, ovalue in result.items():
+                single_result = []
+                for ivalue in ovalue.values():
+                    single_result.append(str(ivalue))
+                total_result.append(': '.join(single_result))
+            result = '\n        '.join(total_result)
         body = f'''
         {result}
 
         credit: yjmd2222's fake-attendance project https://github.com/yjmd2222/fake-attendance
         '''
-        self.body = dedent(body).strip()
+        return dedent(body).strip()
 
-    def run(self):
+    def send_email(self, body):
         'send email'
         try:
             smtp = smtplib.SMTP(SMTP_HOST, SMTP_PORT)
@@ -79,7 +74,7 @@ class Notify(BaseClass):
             print_with_time('이메일 로그인 정보 확인 필요')
             return
 
-        msg = MIMEText(self.body)
+        msg = MIMEText(body)
         msg['Subject'] = f'!!fake-attendance {self.job_id}\
             {datetime.now().strftime(r"%Y-%m-%d %H:%M")}!!'
 
@@ -89,21 +84,43 @@ class Notify(BaseClass):
 
         smtp.quit()
 
-# pylint: disable=too-few-public-methods
-class SendEmail:
+    def run(self, result:dict=None):
+        'run'
+        # write email body
+        body = self.write_body(result)
+        # send email
+        self.send_email(body)
+
+class PrepareSendEmail:
     'A class for instantiating Notify class'
 
-    # pylint: disable=no-member
-    def __init__(self):
+    # pylint: disable=attribute-defined-outside-init,no-member
+    def define_attributes(self):
         '''
-        SendEmail.__init__(self) method that defines\n
+        PrepareSendEmail.define_attributes() that defines\n
         a notify attribute with an instance of Notify\n
         and a result_dict attribute for multiple purposes
         '''
         self.notify = Notify(self.print_name)
         self.result_dict: dict = RESULT_DICTS[self.print_name]
+
+    def decorator_send_email_reset(self, func):
+        'decorator for sending email and resetting attributes'
+        def wrapper(*args, **kwargs):
+            func(*args, **kwargs)
+            self.notify.run(self.result_dict)
+            self.reset_attributes()
+        return wrapper
     # pylint: enable=no-member
-# pylint: enable=too-few-public-methods
+
+    def decorate_run(self):
+        '''PrepareSendEmail.decorate_run() that decorates self.run()\n
+        to send email and reset attributes'''
+        self.run = self.decorator_send_email_reset(self.run)
+    # pylint: enable=attribute-defined-outside-init
+
+    def reset_attributes(self):
+        'base method for resetting attributes at the end'
 
 if __name__ == '__main__':
     Notify().run()

fake_attendance/quit_zoom.py
@@ -12,17 +12,20 @@ sys.path.append(os.getcwd())
 # pylint: disable=wrong-import-position
 from fake_attendance.abc import BaseClass
 from fake_attendance.helper import print_with_time
-from fake_attendance.notify import SendEmail
+from fake_attendance.notify import PrepareSendEmail
 from fake_attendance.settings import ZOOM_CLASSROOM_CLASS
 # pylint: enable=wrong-import-position
 
-class QuitZoom(SendEmail, BaseClass):
+class QuitZoom(PrepareSendEmail, BaseClass):
     'A class for quitting Zoom'
 
     def __init__(self):
         self.print_name = '줌 종료'
-        SendEmail.__init__(self)
+        PrepareSendEmail.define_attributes(self)
         BaseClass.__init__(self)
+
+    def reset_attributes(self):
+        'not implemented'
 
     def connect_and_kill(self, kill_hidden):
         'connect to Zoom conference and kill'
@@ -49,9 +52,7 @@ class QuitZoom(SendEmail, BaseClass):
 
         # send email if called at end of session
         if not kill_hidden:
-            self.notify.record_result(self.result_dict)
-            self.notify.write_body()
-            self.notify.run()
+            self.notify.run(self.result_dict)
 
 if __name__ == '__main__':
     QuitZoom().run()

fake_attendance/helper.py
@@ -11,18 +11,6 @@ import pyautogui
 from win32com.client import Dispatch
 import win32gui
 
-def decorator_start_end(name):
-    'outer decorator, argument should be name for script'
-    def inner_decorator(func):
-        'inner'
-        def wrapper(*args, **kwargs):
-            'wrapper'
-            print_with_time(f'{name} 스크립트 시작')
-            func(*args, **kwargs)
-            print_with_time(f'{name} 스크립트 종료')
-        return wrapper
-    return inner_decorator
-
 def get_file_path(filename, sub=None):
     'return full file path'
     if sub:

fake_attendance/scheduler.py
@@ -19,7 +19,7 @@ sys.path.append(os.getcwd())
 
 # pylint: disable=wrong-import-position
 from fake_attendance.abc import BaseClass
-from fake_attendance.arg_parse import args
+from fake_attendance.arg_parse import parsed_args
 from fake_attendance.fake_check_in import FakeCheckIn
 from fake_attendance.helper import print_with_time
 from fake_attendance.launch_zoom import LaunchZoom
@@ -175,28 +175,28 @@ class MyScheduler(BaseClass):
             return parsed_time_sets, is_success
 
         # check argument passed. overrided in the order of predefined, text file, and time input
-        if args:
+        if parsed_args:
             # this str will be checked whether to extrapolate or not
             is_success = 'false'
             # predefined time sets
-            if args.predefined:
+            if parsed_args.predefined:
                 # look up time sets map with argument
                 try:
-                    time_sets = ARGUMENT_MAP[args.predefined]
-                    print_with_time(f'사전 정의 스케줄: {args.predefined}')
+                    time_sets = ARGUMENT_MAP[parsed_args.predefined]
+                    print_with_time(f'사전 정의 스케줄: {parsed_args.predefined}')
                     is_success = 'pass'
                 except KeyError:
                     print_with_time(f'사전 정의 스케줄 옵션 잘 못 입력함. {list(ARGUMENT_MAP)} 중 입력해야 함')
                     is_success = 'false'
             # text file
-            if is_success == 'false' and (args.textfile and '.txt' in args.textfile):
-                with open (args.textfile, 'r', encoding='utf-8') as file:
+            if is_success == 'false' and (parsed_args.textfile and '.txt' in parsed_args.textfile):
+                with open (parsed_args.textfile, 'r', encoding='utf-8') as file:
                     raw_time_sets = [time_set.strip() for time_set in file.readlines()[1:]]
                     time_sets, is_success = parse_time(raw_time_sets)
             # check time input
-            if is_success == 'false' and args.time:
-                time_sets, is_success = parse_time(args.time)
-            if args.extrapolate:
+            if is_success == 'false' and parsed_args.time:
+                time_sets, is_success = parse_time(parsed_args.time)
+            if parsed_args.extrapolate:
                 if is_success == 'true':
                     # pylint: disable=wrong-import-position,import-outside-toplevel
                     from fake_attendance.helper import extrapolate_time_sets, unfoil_time_sets

commit 105d6a46e442a71fae95a357b0325122943af5b6

Fix regression that always sends email on each job introduced in #57 (#58)
* send email only if success with self.is_send

* fix lint and reduce code with decorator_start_end

* is_send should be not kill_hidden

* added more predefined schedule identifiers and run 1 PM start check
10 minutes earlier

fake_attendance/abc.py
@@ -21,13 +21,11 @@ from fake_attendance.settings import VERBOSITY__INFO
 class BaseClass(ABC):
     'base class for abstraction'
 
-    # pylint: disable=no-member
     @abstractmethod
     def __init__(self):
         'BaseClass.__init__() that decorates self.run() to print start and end of it'
         # to be used with super().__init__() in subclass
-        self.run = self.decorator_start_end(self.print_name)(self.run)
-    # pylint: enable=no-member
+        self.run = self.decorator_start_end(self.run)
 
     # pylint: disable=method-hidden
     @abstractmethod
@@ -35,18 +33,16 @@ class BaseClass(ABC):
         'run everything'
     # pylint: enable=method-hidden
 
-    @staticmethod
-    def decorator_start_end(name):
-        'outer decorator, argument should be name for script'
-        def inner_decorator(func):
-            'inner'
-            def wrapper(*args, **kwargs):
-                'wrapper'
-                print_with_time(f'{name} 스크립트 시작')
-                func(*args, **kwargs)
-                print_with_time(f'{name} 스크립트 종료')
-            return wrapper
-        return inner_decorator
+    # pylint: disable=no-member
+    def decorator_start_end(self, func):
+        'decorator for printing start and end of script'
+        def wrapper(*args, **kwargs):
+            'wrapper'
+            print_with_time(f'{self.print_name} 스크립트 시작')
+            func(*args, **kwargs)
+            print_with_time(f'{self.print_name} 스크립트 종료')
+        return wrapper
+    # pylint: enable=no-member
 
 class UseSelenium(BaseClass):
     'base class for subclasses that use Selenium'

fake_attendance/arg_parse.py
@@ -5,14 +5,14 @@ parses arguments from command line
 import argparse
 
 # pylint: disable=wrong-import-position
-from fake_attendance.settings import VERBOSITY__INFO
+from fake_attendance.settings import ARGUMENT_MAP, VERBOSITY__INFO
 # pylint: enable=wrong-import-position
 
 parser = argparse.ArgumentParser(description='자동 출석 프로그램. -p, -t, time 중 하나만 선택')
 
 parser.add_argument('time', type=str, nargs='*', help=r'%%H:%%M 형식으로 QR 체크 시간 입력')
 parser.add_argument('-p', '--predefined', type=str,\
-                    help='regular, sprint challenge, project day 1, project days 2-5 중 입력')
+                    help=', '.join(ARGUMENT_MAP.keys()) + ' 중 입력')
 parser.add_argument('-t', '--textfile', type=str, help='.txt 텍스트 파일 이름 입력')
 parser.add_argument('-e', '--extrapolate', action='store_true',
                     help='입력한 시간 포함 주위 5분, 10분 단위로 출석 체크')

fake_attendance/fake_check_in.py
@@ -231,6 +231,7 @@ class FakeCheckIn(PrepareSendEmail, UseSelenium):
 
         return is_continue
 
+    # pylint: disable=attribute-defined-outside-init
     def run(self):
         'run whole check-in process'
         # get Zoom window
@@ -279,7 +280,10 @@ class FakeCheckIn(PrepareSendEmail, UseSelenium):
             until = datetime.now() + timedelta(minutes=30)
             self.sched_drop_runs_until(self.print_name, until)
             print_with_time(f'출석 확인. {datetime.strftime(until, "%H:%M")}까지 출석 체크 실행 안 함')
+            # checker bool to send email
+            self.is_send = True
         return
+    # pylint: enable=attribute-defined-outside-init
 # pylint: enable=too-many-instance-attributes
 
 if __name__ == '__main__':

fake_attendance/launch_zoom.py
@@ -192,6 +192,7 @@ class LaunchZoom(PrepareSendEmail, UseSelenium):
         return self.result_dict[window_name]['content']
     # pylint: enable=too-many-arguments
 
+    # pylint: disable=attribute-defined-outside-init
     def run(self):
         'Run the launch'
         self.quit_zoom() # kill hidden Zoom conference windows if any
@@ -205,6 +206,9 @@ class LaunchZoom(PrepareSendEmail, UseSelenium):
         # maximize if everything done correctly
         if self.result_dict[ZOOM_AGREE_RECORDING_POPUP_CLASS]['content']:
             self.maximize_window(self.hwnd_zoom_classroom)
+        # chekcer bool to send email. See PrepareSendEmail.decorator_send_email_reset()
+        self.is_send = True
+    # pylint: enable=attribute-defined-outside-init
 
 if __name__ == '__main__':
     LaunchZoom().run()

fake_attendance/notify.py
@@ -103,12 +103,14 @@ class PrepareSendEmail:
         '''
         self.notify = Notify(self.print_name)
         self.result_dict: dict = RESULT_DICTS[self.print_name]
+        self.is_send = False
 
     def decorator_send_email_reset(self, func):
         'decorator for sending email and resetting attributes'
         def wrapper(*args, **kwargs):
             func(*args, **kwargs)
-            self.notify.run(self.result_dict)
+            if self.is_send:
+                self.notify.run(self.result_dict)
             self.reset_attributes()
         return wrapper
     # pylint: enable=no-member

fake_attendance/quit_zoom.py
@@ -20,12 +20,15 @@ class QuitZoom(PrepareSendEmail, BaseClass):
     'A class for quitting Zoom'
 
     def __init__(self):
+        'initialize'
         self.print_name = '줌 종료'
         PrepareSendEmail.define_attributes(self)
+        PrepareSendEmail.decorate_run(self)
         BaseClass.__init__(self)
 
     def reset_attributes(self):
-        'not implemented'
+        'reset attributes for next run'
+        PrepareSendEmail.define_attributes(self)
 
     def connect_and_kill(self, kill_hidden):
         'connect to Zoom conference and kill'
@@ -46,13 +49,17 @@ class QuitZoom(PrepareSendEmail, BaseClass):
             self.result_dict['quit']['content'] = False
             return False
 
+    # pylint: disable=attribute-defined-outside-init
     def run(self, kill_hidden=False):
-        'Run the launch'
+        '''
+        Run the launch. kill_hidden==True quits hidden Zoom conf before launching.\n
+        kill_hidden==False quits Zoom at the end of session and sends email
+        '''
         self.connect_and_kill(kill_hidden)
 
-        # send email if called at end of session
-        if not kill_hidden:
-            self.notify.run(self.result_dict)
+        # checker bool to send email
+        self.is_send = not kill_hidden
+    # pylint: enable=attribute-defined-outside-init
 
 if __name__ == '__main__':
     QuitZoom().run()

fake_attendance/settings.py
@@ -35,28 +35,40 @@ START_IMAGE = get_file_path('start_video.png', 'images')
 # 11:20, 13:00, 15:20 normal
 # 11:50, 13:00, 15:50 sprint challenge
 # 10:25(Days 2-5) 11:00(Day 1), 13:00, 15:30 project
+# 10:50, 13:00, 15:25 section review Day 1
+# 10:30, 13:00, 14:50 section review Day 2
 DIFF_MINUTE = 5
 REGULAR_CHECK_IN_TIMES = unfoil_time_sets(
-    [extrapolate_time_sets(TIME_, diff_minute=DIFF_MINUTE)\
-     for TIME_ in ['11:20', '13:11', '15:20']]
+    [extrapolate_time_sets(TIME_, diff_minute=DIFF_MINUTE)
+     for TIME_ in ['11:20', '13:01', '15:20']]
 )
 SC_CHECK_IN_TIMES = unfoil_time_sets(
-    [extrapolate_time_sets(TIME_, diff_minute=DIFF_MINUTE)\
-     for TIME_ in ['11:50', '13:11', '15:50']]
+    [extrapolate_time_sets(TIME_, diff_minute=DIFF_MINUTE)
+     for TIME_ in ['11:50', '13:01', '15:50']]
 )
 P_D1_CHECK_IN_TIMES = unfoil_time_sets(
-    [extrapolate_time_sets(TIME_, diff_minute=DIFF_MINUTE)\
-     for TIME_ in ['11:00', '13:11', '15:20']]
+    [extrapolate_time_sets(TIME_, diff_minute=DIFF_MINUTE)
+     for TIME_ in ['11:00', '13:01', '15:20']]
 )
 P_D2_5_CHECK_IN_TIMES = unfoil_time_sets(
-    [extrapolate_time_sets(TIME_, diff_minute=DIFF_MINUTE)\
-     for TIME_ in ['10:30', '13:11', '15:20']]
+    [extrapolate_time_sets(TIME_, diff_minute=DIFF_MINUTE)
+     for TIME_ in ['10:30', '13:01', '15:20']]
+)
+SR_D1_CHECK_IN_TIMES = unfoil_time_sets(
+    [extrapolate_time_sets(TIME_, diff_minute=DIFF_MINUTE)
+     for TIME_ in ['10:50', '13:01', '15:25']]
+)
+SR_D2_CHECK_IN_TIMES = unfoil_time_sets(
+    [extrapolate_time_sets(TIME_, diff_minute=DIFF_MINUTE)
+     for TIME_ in ['10:30', '13:01', '14:50']]
 )
 ARGUMENT_MAP = {
     'regular': REGULAR_CHECK_IN_TIMES,
     'sprint challenge': SC_CHECK_IN_TIMES,
     'project day 1': P_D1_CHECK_IN_TIMES,
-    'project days 2-5': P_D2_5_CHECK_IN_TIMES
+    'project days 2-5': P_D2_5_CHECK_IN_TIMES,
+    'section review day 1': SR_D1_CHECK_IN_TIMES,
+    'section review day 2': SR_D2_CHECK_IN_TIMES
 }
 ZOOM_ON_TIMES = [convert_to_datetime(TIME_) for TIME_ in ['8:59', '12:59']]
 ZOOM_QUIT_TIMES = [convert_to_datetime(TIME_) for TIME_ in ['12:05', '18:05']]